502f253f3dc23b13f0574b49857b8ef02376c852
Xathor <andymarcel.97@gmail.com>
2021-03-21 17:10:00 +0100

    Initial commit.

---
+++ src/test/java/com/xathordroid/reactspringrest/ReactSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xathordroid.reactspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
