e24c6ec91cb4446cd86807370d618256bd4ff353
Xathor <andymarcel.97@gmail.com>
2021-03-21 20:52:53 +0100

    Add more default employees to database loader.

--- src/main/java/com/xathordroid/reactspringrest/loader/DatabaseLoader.java
+++ src/main/java/com/xathordroid/reactspringrest/loader/DatabaseLoader.java
@@ -19,5 +19,9 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.repository.save(new Employee("Bilbo", "Baggins", "burglar"));
+        this.repository.save(new Employee("Sandra", "Tests", "burglar"));
+        this.repository.save(new Employee("Another", "Bolson", "burglar"));
+        this.repository.save(new Employee("Quinto", "Usuario", "burglar"));
     }
 }
